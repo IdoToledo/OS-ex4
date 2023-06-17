fce7e47e5fd48adb3d766a5353fd47324465a3f8
IdoToledo <idot1311@gmail.com>
2023-06-17 15:54:57 +0300

    Changes by Yuval

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -50,9 +50,6 @@ uint64_t readBits(uint64_t virtualAddress, uint64_t bitsRead, uint64_t fixedVirt
 
     // Translating the bits to a value between 2^0 and 2^OFFSET_WIDTH
     return returnVal >> (fixedVirtualAddress - OFFSET_WIDTH - bitsRead);
-
-
-//    return (virtualAddress & ((1LL << (rawBits+1))-1)) >> (VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH - bitsRead);
 }
 
 void cyclicalTraverse(int frameNum, int depth, long long& cyclicalDistance,
@@ -97,12 +94,12 @@ void cyclicalTraverse(int frameNum, int depth, long long& cyclicalDistance,
 
 int treeTraverse(int frameNum, int depth, int& maxFrame, int* frameVisited, int prevFrame, int prevFrameIndex)
 {
-    pageNum = pageNum << ((long long) OFFSET_WIDTH);
     if (frameNum > maxFrame)
     {
         maxFrame = frameNum;
     }
-    if (depth == TABLES_DEPTH)
+
+    if (depth == TABLES_DEPTH) // In a leaf
     {
         return 0;
     }
@@ -138,7 +135,7 @@ int treeTraverse(int frameNum, int depth, int& maxFrame, int* frameVisited, int 
     return 0;
 }
 
-int makeRoom(int pageNum, int* frameVisited)
+int makeRoom(int* frameVisited, long long pageSwappedIn)
 {
     int maxFrame = 0;
     int f = treeTraverse(0, 0, maxFrame, frameVisited, -1, 0);
@@ -150,6 +147,18 @@ int makeRoom(int pageNum, int* frameVisited)
     {
         return maxFrame + 1;
     }
+
+    long long cyclicalDistance = -1;
+    long long cyclicalPage = 0;
+    long long childIndex = 0;
+    long long frameIndex = 0;
+
+    cyclicalTraverse(0, 0, cyclicalDistance, pageSwappedIn, 0, cyclicalPage, childIndex, frameIndex);
+    int frameIndexChild;
+    PMread((frameIndex * PAGE_SIZE) + childIndex, &frameIndexChild);
+    PMevict(frameIndexChild, cyclicalPage);
+    PMwrite((frameIndex * PAGE_SIZE) + childIndex, 0);
+    return frameIndexChild;
 }
 
 void initializeFramesVisited(int* frameVisited)
@@ -161,51 +170,79 @@ void initializeFramesVisited(int* frameVisited)
     }
 }
 
+
+uint64_t VMhandler(uint64_t virtualAddress, int fixedVirtualAddress)
+{
+
+    // Calculate how many bits are not processed yet
+    word_t addr1;
+    uint64_t currentRow = 0;
+    int frameVisited[TABLES_DEPTH+1];
+    long long pageNumber = 0;
+    initializeFramesVisited(frameVisited);
+    long long pageSwappedIn = virtualAddress >> OFFSET_WIDTH;
+    int k = 0;
+
+    // Reading from tree
+    for (int i = 0; i < fixedVirtualAddress - OFFSET_WIDTH; i += OFFSET_WIDTH)
+    {
+        pageNumber = pageNumber << ((long long) OFFSET_WIDTH);
+
+        pageNumber += (long long)readBits(virtualAddress, i, fixedVirtualAddress) ;
+        PMread(currentRow + readBits(virtualAddress, i, fixedVirtualAddress), &addr1);
+        if (addr1 == NOT_IN_MEMORY)
+        {
+            // Finding virtual page num
+            addr1 = makeRoom(frameVisited, pageSwappedIn);
+            // Writing to papa his son/daughter
+            PMwrite(currentRow + readBits(virtualAddress, i, fixedVirtualAddress), addr1);
+            if (i + OFFSET_WIDTH >= fixedVirtualAddress - OFFSET_WIDTH)
+            {
+                PMrestore(addr1, pageNumber);
+            }
+            else
+            {
+                // Making Rows in Frame 0
+                for (int j = 0; j < PAGE_SIZE; j++)
+                {
+                    PMwrite(addr1 * PAGE_SIZE + j, 0);
+                }
+            }
+        }
+        // Current Frame!
+        currentRow = addr1 * PAGE_SIZE;
+        frameVisited[++k] = addr1;
+    }
+
+    return currentRow;
+}
+
+
 int VMread(uint64_t virtualAddress, word_t* value)
 {
-    uint64_t currentFrame = helper(virtualAddress);
+    if (virtualAddress >= VIRTUAL_MEMORY_SIZE || value == nullptr)
+    {
+        return 0;
+    }
     int fixedVirtualAddress = VIRTUAL_ADDRESS_WIDTH + (VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH);
+    uint64_t currentRow = VMhandler(virtualAddress, fixedVirtualAddress);
 
     // Reading from root
-    PMread(currentFrame + readBits(virtualAddress, fixedVirtualAddress - OFFSET_WIDTH, fixedVirtualAddress), value);
+    PMread(currentRow + readBits(virtualAddress, fixedVirtualAddress - OFFSET_WIDTH, fixedVirtualAddress), value);
     return 1;
 }
 
 
 int VMwrite(uint64_t virtualAddress, word_t value)
 {
-    // Don't need to check virtualAddress < RAM_SIZE
-
-    // Calculate how many bits are not processed yet
-    word_t addr1;
-    word_t addr2;
-    uint64_t currentFrame = 0;
-    int frameVisited[TABLES_DEPTH+1];
-    initializeFramesVisited(frameVisited);
-    int fixedVirtualAddress = VIRTUAL_ADDRESS_WIDTH + (VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH);
-    int j = 0;
-    // Reading from tree
-    for (int i = 0; i < fixedVirtualAddress - OFFSET_WIDTH; i += OFFSET_WIDTH)
+    if (virtualAddress >= VIRTUAL_MEMORY_SIZE)
     {
-        PMread(currentFrame + readBits(virtualAddress, i, fixedVirtualAddress), &addr1);
-        if (addr1 == NOT_IN_MEMORY)
-        {
-            // Finding virtual page num
-            addr1 = makeRoom(ROOT, frameVisited);
-            // Writing to papa his son/daughter
-            PMwrite(currentFrame + readBits(virtualAddress, i, fixedVirtualAddress), addr1);
-        }
-        // Current Frame!
-        currentFrame = addr1 * PAGE_SIZE;
-        // Making Rows in Frame 0
-        for (int j = 0; j < PAGE_SIZE; j++)
-        {
-            PMwrite(currentFrame + j, 0);
-        }
-        frameVisited[++j] = addr1;
+        return 0;
     }
+    int fixedVirtualAddress = VIRTUAL_ADDRESS_WIDTH + (VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH);
+    uint64_t currentRow = VMhandler(virtualAddress, fixedVirtualAddress);
 
     // Reading from root
-    PMwrite(currentFrame + readBits(virtualAddress, fixedVirtualAddress - OFFSET_WIDTH, fixedVirtualAddress), value);
+    PMwrite(currentRow + readBits(virtualAddress, fixedVirtualAddress - OFFSET_WIDTH, fixedVirtualAddress), value);
     return 1;
 }
